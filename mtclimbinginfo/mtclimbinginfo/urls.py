0073507afa390ac52d52284631f6f43e8bfcb5dc
hondatoshihiro <harutokaho@gmail.com>
2022-05-30 23:53:51 +0900

    add index view

--- mtclimbinginfo/mtclimbinginfo/urls.py
+++ mtclimbinginfo/mtclimbinginfo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import searchnoboro.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('searchnoboro/', searchnoboro.views.index),
 ]
--- mtclimbinginfo/searchnoboro/views.py
+++ mtclimbinginfo/searchnoboro/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Noboro
 
 # Create your views here.
+
+def index(request):
+    data = Noboro.objects.all()
+    params = {
+        'title': 'Search Noboro',
+        'message': 'all Noboros.',
+        'data': data,
+    }
+    return render(request, 'searchnoboro/index.html', params)
