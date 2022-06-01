f5f6f54b26f8421a9dcd1a01b502a4b47f03ccff
hondatoshihiro <harutokaho@gmail.com>
2022-06-01 22:14:27 +0900

    コメント追加

--- mtclimbinginfo/mtclimbinginfo/urls.py
+++ mtclimbinginfo/mtclimbinginfo/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('searchnoboro/', searchnoboro.views.index),
+    #ここではsearchnoboro appのurls.pyをインクルードする
     path('searchnoboro/', include('searchnoboro.urls')),
 ]
--- mtclimbinginfo/searchnoboro/urls.py
+++ mtclimbinginfo/searchnoboro/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #searchnoboro app内のurlを記載(http://localhost:8000/searchnoboro/～)
     path('', views.index, name='index'),
 ]
