e2c58e36fda3b31e8114e9a0a8981f3bd63be639
hondatoshihiro <harutokaho@gmail.com>
2022-06-01 16:11:08 +0900

    app毎のurls登録

--- mtclimbinginfo/mtclimbinginfo/urls.py
+++ mtclimbinginfo/mtclimbinginfo/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import searchnoboro.views
+from django.urls import path,include
+#import searchnoboro.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('searchnoboro/', searchnoboro.views.index),
+    #path('searchnoboro/', searchnoboro.views.index),
+    path('searchnoboro/', include('searchnoboro.urls')),
 ]
