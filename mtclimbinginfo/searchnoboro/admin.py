aee3b8e4d546d6cd3ed1de70d5678c6a75424ca1
hondatoshihiro <harutokaho@gmail.com>
2022-06-12 12:52:13 +0900

    NoboroContentの追加

--- mtclimbinginfo/searchnoboro/admin.py
+++ mtclimbinginfo/searchnoboro/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Noboro
+from .models import NoboroContent
 
 # Register your models here.
 admin.site.register(Noboro)
+admin.site.register(NoboroContent)
